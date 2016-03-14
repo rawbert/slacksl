6132bc0137fa1922aa60e09a0011c811b0283bc3
rawbert <robert.andersson@gmail.com>
2016-03-14 21:34:18 +0100

    Preppar för slack

--- app/index.js
+++ app/index.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 var http = require('http');
 var dispatcher = require('httpdispatcher');
-var url = require('url');
-var queryString = require('querystring');
+var querystring = require('querystring');
 
 var sl = require('./lib/sl');
 
@@ -10,106 +9,78 @@ function handleRequest(request, response){
     try {
         //log the request on console
         console.log(request.url);
-        //Disptach
         dispatcher.dispatch(request, response);
     } catch(err) {
         console.log(err);
     }
 }
 
-//A sample GET request    
-dispatcher.onGet("/realtime", function(req, res) {
-    var requestUrl = url.parse(req.url);
-    var queryObj = queryString.parse(requestUrl.query);
-    var jsonResponse = { text: "Hittade inte några avångar."};
+dispatcher.onGet('/realtime', function(req, res) {
     res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
-    if(!queryObj || !queryObj.station || queryObj.station === '') {
-        res.end(JSON.stringify(jsonResponse));
-        return;
-    }
-    
-    sl.stationSearch(queryObj.station, function parseStationSearch(data) {
-        var stationResponse = JSON.parse(data);        
-        if(!stationResponse) {
-            res.end(JSON.stringify(jsonResponse));
-            return;
-        }
-        
-        if(stationResponse.StatusCode !== 0) {
-            jsonResponse.text = stationResponse.Message;
-            res.end(JSON.stringify(jsonResponse));
-            return;
-        }
-        
-        if(stationResponse.ResponseData !== null && stationResponse.ResponseData.length > 0) {
-            var first = stationResponse.ResponseData[0];
-            jsonResponse.text = first.Name + " - " + first.SiteId;
-            
-            sl.realtime(first.SiteId, function parseRealtime(data) {
-                var realtimeResponse = JSON.parse(data);
-                if(!realtimeResponse) {
-                    jsonResponse.text = 'Kunde inte läsa realtidsinformation';
-                    res.end(JSON.stringify(jsonResponse));
-                }
-                
-                if(realtimeResponse.StatusCode !== 0) {
-                    jsonResponse.text = realtimeResponse.Message;
-                    res.end(JSON.stringify(jsonResponse));
-                }
-                
-                var trainText = "";                
-                
-                realtimeResponse.ResponseData.Trains.forEach(function(element) {
-                    trainText+= "\n"+element.Destination + ": " + element.DisplayTime;
-                }, this);
-                
-                jsonResponse.text = trainText;
-                res.end(JSON.stringify(jsonResponse));
-            })
-
-        } else {
-            jsonResponse.text = 'Kunde inte hitta några stationer som innehåller namnet ' + queryObj.station;
-            res.end(JSON.stringify(jsonResponse));
-        }
-    })
-}); 
-
-var httpserver = http.createServer(handleRequest);
-var httpsserver = http.createServer(handleRequest);
-var port = process.env.PORT || 8080;
-//Lets start our server
-httpserver.listen(port, function(){
-    console.log("Server listening on: http://localhost:%s", port);
+    var jsonResponse = { text: "GET: Hittade inte några avgångar."};
+    res.end(JSON.stringify(jsonResponse));
 });
 
-// httpsserver.listen(HTTPS_PORT, function(){
-//     console.log("Server listening on: https://localhost:%s", HTTPS_PORT);
-// });
-
-function stationSearch(searchstring) {
-    sl.stationSearch('searchstring', function showStationResponse (data) {
-        var stationResponse = JSON.parse(data);
-        if(stationResponse.StatusCode !== 0) {
-            //console.log(stationResponse.Message);
-            return stationResonse.Message;
-        }
+dispatcher.onPost("/realtime", function(req, res) {
+    var form = querystring.parse(req.body);
+    res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
+    
+    var jsonResponse = { text: "POST: Hittade inte några avgångar."};
+    res.end(JSON.stringify(jsonResponse));  
+    // if(!queryObj || !queryObj.station || queryObj.station === '') {
+    //     res.end(JSON.stringify(jsonResponse));
+    //     return;
+    // }
+    
+    // sl.stationSearch(queryObj.station, function parseStationSearch(data) {
+    //     var stationResponse = JSON.parse(data);        
+    //     if(!stationResponse) {
+    //         res.end(JSON.stringify(jsonResponse));
+    //         return;
+    //     }
         
-        if(stationResponse.ResponseData !== null && stationResponse.ResponseData.length > 0) {
-            var first = stationResponse.ResponseData[0];
-            //console.log("Displaying realtime for station: " + first.Name + " - " + first.SiteId);
-            sl.realtime(first.SiteId, function showRealtime(data) {
-                var realtimeResponse = JSON.parse(data);
-                if(realtimeResponse.StatusCode !== 0) {
-                    //console.log(realtimeResponse.Message);
-                    return realtimeResponse.Message;
-                }
+    //     if(stationResponse.StatusCode !== 0) {
+    //         jsonResponse.text = stationResponse.Message;
+    //         res.end(JSON.stringify(jsonResponse));
+    //         return;
+    //     }
+        
+    //     if(stationResponse.ResponseData !== null && stationResponse.ResponseData.length > 0) {
+    //         var first = stationResponse.ResponseData[0];
+    //         jsonResponse.text = first.Name + " - " + first.SiteId;
             
-                var trainData = realtimeResponse.ResponseData.Trains;
-                return trainData;
-                // trainData.forEach(function(element) {
-                //     console.log(element.Destination + ": " + element.DisplayTime);
-                // }, this);    
-            });
-        }
-    });
-}
+    //         sl.realtime(first.SiteId, function parseRealtime(data) {
+    //             var realtimeResponse = JSON.parse(data);
+    //             if(!realtimeResponse) {
+    //                 jsonResponse.text = 'Kunde inte läsa realtidsinformation';
+    //                 res.end(JSON.stringify(jsonResponse));
+    //             }
+                
+    //             if(realtimeResponse.StatusCode !== 0) {
+    //                 jsonResponse.text = realtimeResponse.Message;
+    //                 res.end(JSON.stringify(jsonResponse));
+    //             }
+                
+    //             var trainText = "";                
+                
+    //             realtimeResponse.ResponseData.Trains.forEach(function(element) {
+    //                 trainText+= "\n"+element.Destination + ": " + element.DisplayTime;
+    //             }, this);
+                
+    //             jsonResponse.text = trainText;
+    //             res.end(JSON.stringify(jsonResponse));
+    //         })
+
+    //     } else {
+    //         jsonResponse.text = 'Kunde inte hitta några stationer som innehåller namnet ' + queryObj.station;
+    //         res.end(JSON.stringify(jsonResponse));
+    //     }
+    // })
+}); 
+
+var server = http.createServer(handleRequest);
+var port = process.env.PORT || 8080;
+//Lets start our server
+server.listen(port, function(){
+    console.log("Server listening on: http://localhost:%s", port);
+});
